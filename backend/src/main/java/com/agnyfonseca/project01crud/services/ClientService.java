402bfcf478caec689b53c8aa38328148f15356d5
Agny <agnyfonseca@outlook.com>
2021-11-24 16:24:53 +0100

    DELETE Client

--- backend/src/main/java/com/agnyfonseca/project01crud/services/ClientService.java
+++ backend/src/main/java/com/agnyfonseca/project01crud/services/ClientService.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.agnyfonseca.project01crud.dto.ClientDTO;
 import com.agnyfonseca.project01crud.entities.Client;
 import com.agnyfonseca.project01crud.repositories.ClientRepository;
+import com.agnyfonseca.project01crud.services.exceptions.DatabaseException;
 import com.agnyfonseca.project01crud.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -62,6 +65,19 @@ public class ClientService {
 		}
 	}
 	
+	//DELETE
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
+	}
+	
 	//METODO AUXILIAR
 	private void copyDtoToEntity(ClientDTO dto, Client entity) {
 		entity.setName(dto.getName());
@@ -70,6 +86,4 @@ public class ClientService {
 		entity.setBirthDate(dto.getBirthDate());
 		entity.setChildren(dto.getChildren());
 	}
-
-	
 }
