e0fa15bd57a2604004eaae3dcbb388563a283845
Agny <agnyfonseca@outlook.com>
2021-11-24 14:42:40 +0100

    Getting all Clients without Pagination

--- backend/src/main/java/com/agnyfonseca/project01crud/resources/ClientResource.java
+++ backend/src/main/java/com/agnyfonseca/project01crud/resources/ClientResource.java
@@ -1,5 +1,29 @@
 package com.agnyfonseca.project01crud.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.agnyfonseca.project01crud.dto.ClientDTO;
+import com.agnyfonseca.project01crud.services.ClientService;
+
+@RestController
+@RequestMapping(value = "/clients")
 public class ClientResource {
+	
+	@Autowired
+	private ClientService service;
+	
+	//GET ALL
+	@GetMapping
+	public ResponseEntity<List<ClientDTO>> findAll() {
+		List<ClientDTO> list = service.findAll();
+		
+		return ResponseEntity.ok().body(list);
+	}
 
 }
--- backend/src/main/java/com/agnyfonseca/project01crud/services/ClientService.java
+++ backend/src/main/java/com/agnyfonseca/project01crud/services/ClientService.java
@@ -1,5 +1,27 @@
 package com.agnyfonseca.project01crud.services;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.agnyfonseca.project01crud.dto.ClientDTO;
+import com.agnyfonseca.project01crud.entities.Client;
+import com.agnyfonseca.project01crud.repositories.ClientRepository;
+
+@Service
 public class ClientService {
+	
+	@Autowired
+	private ClientRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<ClientDTO> findAll() {
+		List<Client> listDto = repository.findAll();
+		
+		return listDto.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
+	}
 
 }
