9fc3f3e504e91a23b0d2f44d6e6cd38b3048dce1
Agny <agnyfonseca@outlook.com>
2021-11-24 12:26:26 +0100

    Project created

---
+++ backend/src/main/java/com/agnyfonseca/project01crud/Project01CrudApplication.java
@@ -0,0 +1,13 @@
+package com.agnyfonseca.project01crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project01CrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project01CrudApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/agnyfonseca/project01crud/Project01CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agnyfonseca.project01crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project01CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
